08ccb8078d076f88deb0422f643e710b5dffd4d6
Zarar Siddiqi <zarars@gmail.com>
2019-07-22 20:41:22 -0400

    Fix PWA title

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Zarar Siddiqi's Blog`,
+        short_name: `Zarar Siddiqi`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.jpg`,
       },
     },
     `gatsby-plugin-offline`,
