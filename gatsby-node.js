5250c2f74a6d4ce2ff5a0b3027d4cdbbcb2a883f
Zarar Siddiqi <zarars@gmail.com>
2020-06-29 16:16:39 -0400

    don't display z on front page

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,6 @@ exports.createPages = async ({ graphql, actions }) => {
     `
       {
         allMarkdownRemark(
-          filter: {fileAbsolutePath: {regex: "/(blog)/"  }}
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -28,50 +27,13 @@ exports.createPages = async ({ graphql, actions }) => {
     `
   )
 
-  const zresult = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          filter: {fileAbsolutePath: {regex: "/(zettelkasten)/"  }}
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  )
 
   if (result.errors) {
     throw result.errors
   }
 
   // Create blog posts pages.
-  const posts = zresult.data.allMarkdownRemark.edges
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
+  const posts = result.data.allMarkdownRemark.edges
 
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
